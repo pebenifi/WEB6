b3abaff37dbea55d208d704f3ed51f0aebffad1e
Тимофей Напольников <pebenifi@gmail.com>
2021-04-22 15:05:42 +0300

    перемещение

--- main.py
+++ main.py
@@ -8,23 +8,29 @@ URL = 'http://static-maps.yandex.ru/1.x/'
 ll = [37.530887, 55.7033118]
 l = 'sat'
 spn = [0.002, 0.002]
+lastll = [34, 34]
 
 def get_image():
+    print('getting_image')
     image = requests.get(URL, params=get_params())
     if image:
         return image
     print(image.content)
+
+def checkState():
+    return lastll != ll
+
 def load_image(image):
+    print('writed')
     with open('map.png', 'wb') as f:
         f.write(image.content)
 
 def show_image(screen):
     screen.blit(pygame.transform.scale(pygame.image.load('map.png'), (width, height)), (0, 0))
-
+    print('yes')
 
 def get_params():
     return {'ll': f"{ll[0]},{ll[1]}", "spn": f"{spn[0]},{spn[1]}", 'l': l}
-
 
 pygame.init()
 size = width, height = 600, 465
@@ -33,24 +39,30 @@ running = 1
 FPS = 10
 clock = pygame.time.Clock()
 while running:
-    pygame.event.wait()
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             running = 0
         elif ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_UP:
-                ll[1] += 0.001
+                ll[1] += spn[0] / 10
+                print(1)
             elif ev.key == pygame.K_DOWN:
-                ll[1] -= 0.001
+                ll[1] -= spn[0] / 10
+                print(1)
             elif ev.key == pygame.K_LEFT:
-                ll[0] -= 0.001
+                ll[0] -= spn[1] / 10
+                print(1)
             elif ev.key == pygame.K_RIGHT:
-                ll[0] += 0.001
-    screen.fill((0, 0, 0))
-    image = get_image()
-    if image:
-        load_image(image)
-        show_image(screen)
+                ll[0] += spn[1] / 10
+                print(1)
+    if checkState():
+        image = get_image()
+        if image:
+            screen.fill((0, 0, 0))
+            load_image(image)
+            show_image(screen)
+            lastll = ll[:]
     pygame.display.flip()
     clock.tick(FPS)
+
 os.remove(name)
